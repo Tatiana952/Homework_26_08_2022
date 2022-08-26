4b30e2f87b497e32833152d16b05c7238b9e5814
Tatiana <lulu.kep@yandex.ru>
2022-08-26 15:01:50 +0400

    dz

---
+++ 066/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int Sum(int m, int n)
+{
+    if (m == n) return m;
+    else if (m > n) return Sum(n, m-1) + m;
+    else return Sum(m, n-1) + n;
+}
+
+Console.WriteLine();
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(a,b));
---
+++ 068/Program.cs
@@ -0,0 +1,21 @@
+﻿int Acker(int m, int n)
+{
+    // if (m == 0) return n+1;
+    // else 
+    if (m > 0 && n == 0) return Acker(m-1, 1);
+    else if (m > 0 && n > 0) return Acker(m-1, Acker(m, n-1));
+    else return n+1;
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+while( a < 0 || b < 0)
+    {
+        Console.WriteLine("Only zero or positive numbers");
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+    }
+
+Console.WriteLine();
+Console.WriteLine(Acker(a,b));
